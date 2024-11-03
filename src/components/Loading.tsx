6528bc6d556319c6f799d29805e3932748cf21b5
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2024-11-03 15:59:04 +0300

    implements loading skeleton indicator

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,9 +1,39 @@
 import React from 'react';
+import {
+    IonItem,
+    IonLabel,
+    IonList,
+    IonListHeader,
+    IonSkeletonText,
+    IonThumbnail,
+  } from '@ionic/react'
 import './Loading.css';
 
 const Loading: React.FC = () => {
   return (
-    <div>Loading...</div>
+    <IonList>
+        <IonListHeader>
+            <IonSkeletonText animated={true} style={{ width: '80px' }}></IonSkeletonText>
+        </IonListHeader>
+        <IonItem>
+            <IonThumbnail slot="start">
+                <IonSkeletonText animated={true}></IonSkeletonText>
+            </IonThumbnail>
+        </IonItem>
+        <IonItem>
+            <IonLabel>
+                <h3>
+                <IonSkeletonText animated={true} style={{ width: '80%' }}></IonSkeletonText>
+                </h3>
+                <p>
+                <IonSkeletonText animated={true} style={{ width: '60%' }}></IonSkeletonText>
+                </p>
+                <p>
+                <IonSkeletonText animated={true} style={{ width: '30%' }}></IonSkeletonText>
+                </p>
+            </IonLabel>
+        </IonItem>
+    </IonList>
   );
 };
 
