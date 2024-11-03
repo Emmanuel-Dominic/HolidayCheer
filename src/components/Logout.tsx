8bc2b0d4854a1d0e01033f98e698333a91c45d19
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2024-11-03 19:37:14 +0300

    add user logout logic

--- src/components/Logout.tsx
+++ src/components/Logout.tsx
@@ -1,14 +1,45 @@
-import React from 'react';
-import { IonButton, IonIcon } from "@ionic/react";
+import React, { useState } from 'react';
+import { IonAlert, IonButton, IonIcon } from "@ionic/react";
 import { logOutOutline } from "ionicons/icons";
+import { useHistory } from 'react-router-dom';
+import { useAuth } from '../AuthContext';
 
 const Logout = () => {
+  const [showAlert, setShowAlert] = useState(false);
+  const history = useHistory();
+  const { currentUser, logout } = useAuth();
+
+  if (!currentUser) return null;
+
+  const handleLogout = async() => {
+    await logout();
+    history.push("/login");
+  };
 
   return (
-    <IonButton color="danger" slot="end" >
-      <IonIcon icon={logOutOutline} slot="start" />
-      Logout
-    </IonButton>
+    <>
+      <IonButton onClick={() => setShowAlert(true)} color="danger" slot="end" >
+        <IonIcon icon={logOutOutline} slot="start" />
+        Logout
+      </IonButton>
+
+      <IonAlert
+        isOpen={showAlert}
+        onDidDismiss={() => setShowAlert(false)}
+        header="Confirm Logout"
+        message="Are you sure you want to log out?"
+        buttons={[
+          {
+            text: "Cancel",
+            role: "cancel",
+          },
+          {
+            text: "Logout",
+            handler: handleLogout,
+          },
+        ]}
+      />
+    </>
   );
 };
 
